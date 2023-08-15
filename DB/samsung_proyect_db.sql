a81f9ad2e2852310f947c9ac71c6707fc54e3564
KeuryLendof <lendofkeury@gmail.com>
2023-08-14 20:43:37 -0400

    update database with table admin

--- DB/samsung_proyect_db.sql
+++ DB/samsung_proyect_db.sql
@@ -39,6 +39,22 @@ CREATE TABLE calificaciones
     Examen_final int NULL
 );
 
+-- Crear la tabla admin
+CREATE TABLE admin (
+    id_admin int IDENTITY(1,1) PRIMARY KEY,
+    nombre varchar(45) NOT NULL,
+    apellido varchar(45) NOT NULL,
+    usuario varchar(45) NOT NULL,
+    contrasena varchar(45) NOT NULL,
+    rol varchar(45) NOT NULL
+);
+
+INSERT INTO admin (nombre, apellido, usuario, contrasena, rol)
+VALUES
+    ('Orga', 'Carderor', '123', '123', 'directora'),
+    ('Meguelina', 'Guzman', 'miguelina_admin', 'adminpass', 'vice-directora'),
+    ('Sergei', 'Diaz', 'sergei_admin', 'password', 'administrador');
+
 -- Agregar la columna Promedio a la tabla calificaciones
 ALTER TABLE calificaciones ADD Promedio AS (Primer_examen + Segundo_examen + Tercer_examen + Examen_final) / 4 PERSISTED;
 
@@ -55,6 +71,14 @@ VALUES ('Carlos', 'Gomez', 'carlos789', '345678');
 -- Insertar un estudiante y asignarle un profesor (reemplaza los valores de ejemplo)
 INSERT INTO alumnos (nombre, apellido, sexo, usuario, contrasena, fk_id_profesores)
 VALUES ('prueba3', '333', 'Femenina', '333', '123', 3);
+
+
+SELECT a.id_alumnos, a.nombre, a.apellido, a.usuario, a.contrasena,
+       CONCAT(p.nombre, ' ', p.apellido) AS profesor
+FROM alumnos a
+INNER JOIN profesores p ON a.fk_id_profesores = p.id_profesores
+WHERE a.id_alumnos = 1;
+
 
 select * from profesores
 select * from alumnos
